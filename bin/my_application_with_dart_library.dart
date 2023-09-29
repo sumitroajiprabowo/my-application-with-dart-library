027b41aa142a8e86add52bae3cc73f1a9914a59d
Sumitro Aji Prabowo <bowo@anakdesa.id>
2023-09-29 21:23:26 +0700

    initial commit

---
+++ bin/my_application_with_dart_library.dart
@@ -0,0 +1,5 @@
+import 'package:my_application_with_dart_library/my_application_with_dart_library.dart' as my_application_with_dart_library;
+
+void main(List<String> arguments) {
+  print('Hello world: ${my_application_with_dart_library.calculate()}!');
+}
---
+++ test/my_application_with_dart_library_test.dart
@@ -0,0 +1,8 @@
+import 'package:my_application_with_dart_library/my_application_with_dart_library.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
